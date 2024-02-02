6a68bc25326d23fce56a4fb50e4c4f91f339ef0e
Elias <simon.eliasv@gmail.com>
2024-02-02 17:12:50 +0100

    added Google Sign in / Sign out

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -52,6 +53,38 @@ void checkLoginStatus() {
   });
 }
 
+Future<UserCredential?> signInWithGoogle() async {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+  // Check if user is null
+  if (googleUser == null) {
+    print('Google Sign In wurde abgebrochen.');
+    return null;
+  }
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+}
+
+Future<void> signOutGoogle() async {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  await googleSignIn.signOut();
+  print("User Signed Out");
+}
+
+Future<void> signOutEmailPassword() async {
+  await FirebaseAuth.instance.signOut();
+  print("Benutzer hat sich bei Firebase abgemeldet");
+}
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
@@ -66,6 +99,11 @@ class MainApp extends StatelessWidget {
             ElevatedButton(onPressed: loginWithEmailAndPassword, child: Text("Login")),
             ElevatedButton(onPressed: registerwithEmailAndPassword, child: Text("Register")),
             ElevatedButton(onPressed: checkLoginStatus, child: Text("CheckLoginState")),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: ElevatedButton(onPressed: signInWithGoogle, child: Text("Google Sign In")),
+            ),
+            ElevatedButton(onPressed: signOutGoogle, child: Text("Google Sign Out")),
           ],
         )),
       ),
